214be0ae1a2b076b2a43959d5e4e9ff541de0fb9
Jungwook Kim <jw0905@naver.com>
2021-01-19 23:32:52 +0900

    14502

---
+++ src/com/backjun/gold/연구소_14502.java
@@ -0,0 +1,24 @@
+package com.backjun.gold;
+
+import java.util.Scanner;
+
+public class 연구소_14502 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N=sc.nextInt();
+		int M=sc.nextInt();
+		int[][] map= new int[N][M];
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				map[i][j]=sc.nextInt();
+			}
+		}
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				map[i][j]=sc.nextInt();
+			}
+		}
+	}
+
+}
