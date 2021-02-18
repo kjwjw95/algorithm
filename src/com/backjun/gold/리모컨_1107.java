f9a2637159ad21782db4255fcc5f0369b76e204c
Jungwook Kim <jw0905@naver.com>
2021-02-18 21:19:35 +0900

    as

--- src/com/backjun/gold/리모컨_1107.java
+++ src/com/backjun/gold/리모컨_1107.java
@@ -1,18 +1,25 @@
 package com.backjun.gold;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class 리모컨_1107 {
-	static int N,M,answer=0;
-	static int[] broken,channel;
+	static int N,M,answer=0,dec;
+	static int[] broken;
+	static ArrayList<Integer> channel;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N=sc.nextInt();
 		M=sc.nextInt();
 		broken = new int[M];
+		channel = new ArrayList<Integer>();
 		while(true) {
-			break;
+			if(N==0)
+				break;
+			channel.add(N%10);
+			N/=10;
 		}
+		dec=channel.size();
 		greater();
 		leater();
 		go();
@@ -20,7 +27,11 @@ public class 리모컨_1107 {
 		
 	}
 	static public void greater() {
-		
+		boolean isUp=false;
+		int temp_ans;//
+		if(isUp) {
+			
+		}
 	}
 	static public void leater() {
 		
