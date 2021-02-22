c18b3a3b8f5bf2e37110a9f0fa59ee5ce3efc58f
Jungwook Kim <jw0905@naver.com>
2021-02-23 00:51:30 +0900

    by

---
+++ src/com/backjun/gold/암호만들기_1759.java
@@ -0,0 +1,17 @@
+package com.backjun.gold;
+
+import java.util.Scanner;
+
+public class 암호만들기_1759 {
+	static int L,C;
+	static char[] pwd;
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		L=sc.nextInt();
+		C=sc.nextInt();
+		pwd = new char[C];
+		for(int i=0;i<C;i++) {
+			pwd[i]
+		}
+	}
+}
