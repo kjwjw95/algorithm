e64ba8d3f9531b6275a54435a98658b61acc42a1
Jungwook Kim <jw0905@naver.com>
2021-02-25 04:43:11 +0900

    clear

--- src/com/backjun/gold/암호만들기_1759.java
+++ src/com/backjun/gold/암호만들기_1759.java
@@ -1,17 +1,55 @@
 package com.backjun.gold;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 암호만들기_1759 {
 	static int L,C;
-	static char[] pwd;
+	static char[] pwd,ans;
+	static boolean[] sel;
+	static char[] moum= {'a','e','i','o','u'};
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		L=sc.nextInt();
 		C=sc.nextInt();
 		pwd = new char[C];
+		sel = new boolean[C];
+		ans= new char[L];
 		for(int i=0;i<C;i++) {
-			pwd[i]
+			pwd[i]=sc.next().charAt(0);
+		}
+		Arrays.sort(pwd);
+		Combination(0,0);
+	}
+	public static void Combination(int idx,int cnt) {
+		if(cnt==L) {
+			int jaum=0,moums=0;
+			for(int i=0;i<L;i++) {
+				boolean isMo=false;
+				for(int j=0;j<5;j++) {
+					if(ans[i]==moum[j]) {
+						isMo=true;
+						moums++;
+						break;
+					}
+				}
+				if(!isMo)
+					jaum++;
+			}
+			if(moums<1 || jaum<2)
+				return;
+			for(int i=0;i<L;i++) {
+				System.out.print(ans[i]);
+			}
+			System.out.println();
+			return;
+		}
+		for(int i=idx;i<C;i++) {
+			if(sel[i])	continue;
+			ans[cnt]=pwd[i];
+			sel[i]=true;
+			Combination(i+1,cnt+1);
+			sel[i]=false;
 		}
 	}
 }
